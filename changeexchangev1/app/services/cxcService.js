c912fa3394ee607c44e1b9eafbaeb79a30681677
Hein <heinschulie@gmail.com>
2015-12-09 16:48:05 +0200

    12_09_UpdatedWPUrl

--- changeexchangev1/app/services/cxcService.js
+++ changeexchangev1/app/services/cxcService.js
@@ -7,7 +7,7 @@
         //var applicationBase = 'http://localhost:16327/';
 
         // *** Development and staging
-        var serviceBase = 'http://localhost/ohsoserious/wp-json/';
+        var serviceBase = 'http://changeexchangedev.azurewebsites.net/';
         var applicationBase = 'http://changeexchangev1.azurewebsites.net//';
 
         // *** Production 
